c002c6b5393f0ee2b8113d2719d9b3ee2ec9bab1
Mitchell Walker <eagleye1144@gmail.com>
2021-10-29 13:12:15 -0600

    YUP install with Projects router and middleware done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,53 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model')
+const router = express.Router();
+const { logger, validateProjectId, validateProject } = require('./projects-middleware.js');
+
+router.get('/', logger, (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(next);
+});
+
+router.get('/:id', logger, validateProjectId, (req, res) => {
+    res.status(200).json(req.project);
+});
+
+router.post('/', logger, validateProject, (req, res, next) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(next);
+});
+
+router.put('/:id', logger, validateProjectId, validateProject, (req, res, next) => {
+    Projects.update(req.params.id, req.body)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(next);
+});
+
+router.delete('/:id', logger, validateProjectId, async (req, res, next) => {
+    try {
+        await Projects.remove(req.params.id);
+        res.status(200).json({
+            message: "project deleted"
+        })
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/:id/actions', logger, validateProjectId, (req, res, next) => {
+    Projects.getProjectActions(req.params.id)
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const server = express();
-
+const projectsRouter = require('./projects/projects-router');
 server.use(express.json());
 
+server.use('/api/projects', projectsRouter);
 
 server.use('/', (req, res) => {
     res.send(`<h2>Sprint API boi</h2>`);
