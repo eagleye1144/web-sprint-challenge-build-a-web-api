9cf577c9085cf44998344a35a4822dd93b41d1d7
Mitchell Walker <eagleye1144@gmail.com>
2021-10-29 13:13:39 -0600

    Actions Router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,52 @@
-// Write your "actions" router here!
+
+const express = require('express');
+const Actions = require('./actions-model');
+const { logger, validateActionId, validateAction } = require('./actions-middleware');
+
+const router = express.Router();
+
+
+router.get('/', logger, (req, res, next) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(next);
+})
+
+
+router.get('/:id', logger, validateActionId, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+
+router.post('/', logger, validateAction, (req, res, next) => {
+    Actions.insert(req.body)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(next);
+});
+
+
+router.put('/:id', logger, validateActionId, validateAction, (req, res, next) => {
+    Actions.update(req.params.id, req.body)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(next);
+});
+
+
+router.delete('/:id', logger, validateActionId, async (req, res, next) => {
+    try {
+        await Actions.remove(req.params.id);
+        res.status(200).json({
+            message: "action deleted"
+        })
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router; 
--- api/server.js
+++ api/server.js
@@ -5,6 +5,14 @@ server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
 
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      note: `server not working correctly`, 
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+
 server.use('/', (req, res) => {
     res.send(`<h2>Sprint API boi</h2>`);
 });
